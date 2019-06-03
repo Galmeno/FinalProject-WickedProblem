1d54b5018926d42e1790164d08b237a8a6bcc277
Lisa Qing <lisaqing98@gmail.com>
2019-06-03 12:46:59 -0700

    edge distances

--- Wicked_Problem.py
+++ Wicked_Problem.py
@@ -1,34 +1,41 @@
-#<METADATA>
+# <METADATA>
 QUIET_VERSION = "0.2"
 PROBLEM_NAME = "Wicked Problem: HIV/AIDS"
 PROBLEM_VERSION = "0.2"
-PROBLEM_AUTHORS = ['H. Kaushik']
+PROBLEM_AUTHORS = ['H. Kaushik', 'L. Qing']
 PROBLEM_CREATION_DATE = "29-MAY-2019"
-PROBLEM_DESC=\
-'''
-This formulation of the wicked problem of disease (with a focus on HIV/AIDS)
-uses generic Python 3 constructs and has been tested with Python 3.6.
-It is designed to work according to the QUIET2 tools interface.
+PROBLEM_DESC = \
+    '''
+    This formulation of the wicked problem of disease (with a focus on HIV/AIDS)
+    uses generic Python 3 constructs and has been tested with Python 3.6.
+    It is designed to work according to the QUIET2 tools interface.
 
-Int_Solve_Client can  be used such that the user can choose an action among 
-valid operations.
-'''
+    Int_Solve_Client can  be used such that the user can choose an action among
+    valid operations.
+    '''
 
 BUDGET = 28000000000
 
 # 8 Regions considered
-REGIONS = {0 : 'East and Southern Africa', 1 : 'Western and Central Africa', 2 : 'Asia and Pacific', 3 : 'Western and Central Europe and North America', \
-    4 : 'Latin America', 5 : 'Eastern Europe and Central Asia', 6 : 'Carribean', 7 : 'Middle East and North Africa', 8 : 'new treatment', 9 : 'budgetary reasons.'}
+REGIONS = {0: 'East and Southern Africa', 1: 'Western and Central Africa', 2: 'Asia and Pacific',
+           3: 'Western and Central Europe and North America', \
+           4: 'Latin America', 5: 'Eastern Europe and Central Asia', 6: 'Carribean', 7: 'Middle East and North Africa',
+           8: 'new treatment', 9: 'budgetary reasons.'}
 
 # Dictionary of conditions relating to HIV/AIDS based on regions
-INIT_DICT = {0: {'cases' : 19600000, 'sf' : 0.0408, 'deaths' : 426666, 'treatment' : 0.66}, 1 : {'cases' : 6100000, 'sf' : 0.0607, 'deaths' : 197333, 'treatment' : 0.40}, \
-    2 : {'cases' : 5200000, 'sf' : 0.0538, 'deaths' : 149333, 'treatment' : 0.53}, 3 : {'cases' : 2200000, 'sf' : 0.0318, 'deaths' : 37351, 'treatment' : 0.76}, \
-    4 : {'cases' : 1800000, 'sf' : 0.0556, 'deaths' : 53329, 'treatment' : 0.61}, 5 : {'cases' : 1400000, 'sf' : 0.0929, 'deaths' : 69334, 'treatment' : 0.36}, \
-    6 : {'cases' : 310000, 'sf' : 0.0484, 'deaths' : 8000, 'treatment' : 0.57}, 7 : {'cases' : 220000, 'sf' : 0.0818, 'deaths' : 9600, 'treatment' : 0.29}}
+INIT_DICT = {0: {'cases': 19600000, 'sf': 0.0408, 'deaths': 426666, 'treatment': 0.66},
+             1: {'cases': 6100000, 'sf': 0.0607, 'deaths': 197333, 'treatment': 0.40}, \
+             2: {'cases': 5200000, 'sf': 0.0538, 'deaths': 149333, 'treatment': 0.53},
+             3: {'cases': 2200000, 'sf': 0.0318, 'deaths': 37351, 'treatment': 0.76}, \
+             4: {'cases': 1800000, 'sf': 0.0556, 'deaths': 53329, 'treatment': 0.61},
+             5: {'cases': 1400000, 'sf': 0.0929, 'deaths': 69334, 'treatment': 0.36}, \
+             6: {'cases': 310000, 'sf': 0.0484, 'deaths': 8000, 'treatment': 0.57},
+             7: {'cases': 220000, 'sf': 0.0818, 'deaths': 9600, 'treatment': 0.29}}
 
 CREATE_INITIAL_STATE = lambda: State(INIT_DICT, 0, 1, 0, -1)
 
-action_costs = {'Research' : 12000000000, 'Drugs' : 8000000000, 'Education' : 6000000000, 'nothing' : 0}
+action_costs = {'Research': 12000000000, 'Drugs': 8000000000, 'Education': 6000000000, 'nothing': 0}
+
 
 class State:
     def __init__(self, d, q, y, yc, rs):
@@ -49,21 +56,29 @@ class State:
         # Produces a textual description of a state.
         # Might not be needed in normal operation with GUIs.
         s = 'Year: ' + str(self.year) + ' Quarter: ' + str(self.quarter) + '. \n'
-        if self.research_start != -1 and self.research_start < 12: s += 'Research in progress.\n'
-        elif self.research_start >= 12: s += 'Research complete.\n'
+        if self.research_start != -1 and self.research_start < 12:
+            s += 'Research in progress.\n'
+        elif self.research_start >= 12:
+            s += 'Research complete.\n'
         s += '$' + str(BUDGET - self.yearly_cost) + ' left to invest this year.\n'
         for i in range(8):
-            s += REGIONS[i] + ': ' + str(self.d[i]['cases']) + ' cases, ' + str(self.d[i]['sf']) + ' spreading factor, ' + \
-                str(self.d[i]['deaths']) + ' deaths, ' + str(self.d[i]['treatment']) + ' percent receiving treatment. \n'
+            s += REGIONS[i] + ': ' + str(self.d[i]['cases']) + ' cases, ' + str(
+                self.d[i]['sf']) + ' spreading factor, ' + \
+                 str(self.d[i]['deaths']) + ' deaths, ' + str(
+                self.d[i]['treatment']) + ' percent receiving treatment. \n'
         return s
 
     def __hash__(self):
         return (self.__str__()).__hash__()
 
+    def edge_distance(self, s2):
+        return 1.0
+
     def copy(self):
         newd = {}
         for i in range(8):
-            newd[i] = {'cases' : self.d[i]['cases'], 'sf' : self.d[i]['sf'], 'deaths' : self.d[i]['deaths'], 'treatment' : self.d[i]['treatment']}
+            newd[i] = {'cases': self.d[i]['cases'], 'sf': self.d[i]['sf'], 'deaths': self.d[i]['deaths'],
+                       'treatment': self.d[i]['treatment']}
         return State(newd, self.quarter, self.year, self.yearly_cost, self.research_start)
 
     def can_move(self, a, loc):
@@ -73,18 +88,17 @@ class State:
                 return False
             if action_costs[a] + yc <= BUDGET:
                 return True
-            else: 
+            else:
                 return False
         except Exception as e:
             print(e)
-
 
     def move(self, a, loc):
         news = self.copy()
         news.quarter += 1
         if news.quarter == 5:
             news.year += 1
-            news.quarter = 1 
+            news.quarter = 1
         if news.quarter == 1:
             news.yearly_cost = 0
         if news.research_start != -1 and (news.year * 4 + news.quarter) - news.research_start > 12:
@@ -94,24 +108,24 @@ class State:
                 news.d[i]['sf'] = round(self.d[i]['sf'] * 0.9, 4)
 
                 if news.d[i]['sf'] > 1:
-                    news.d[loc]['sf']= 0.999
-            
+                    news.d[loc]['sf'] = 0.999
+
         if a == 'Research':
             news.research_start = news.year * 4 + news.quarter
             news.yearly_cost += action_costs['Research']
-            
+
         elif a == 'Drugs':
             news.d[loc]['deaths'] = int(self.d[loc]['deaths'] * 0.9)
             news.d[loc]['treatment'] = round(self.d[loc]['treatment'] * 1.3, 3)
             news.d[loc]['sf'] = round(self.d[loc]['sf'] * 0.9, 4)
             news.yearly_cost += action_costs['Drugs']
-            #exit(0)
+            # exit(0)
         elif a == 'Education':
             news.d[loc]['sf'] = round(self.d[loc]['sf'] * 0.7, 4)
             news.d[loc]['treatment'] = round(self.d[loc]['treatment'] * 1.1, 3)
             news.d[loc]['cases'] = int(self.d[loc]['cases'] * 0.9)
             news.yearly_cost += action_costs['Education']
-            #exit(0)
+            # exit(0)
 
         if loc < 8:
             if news.d[loc]['sf'] > 1:
@@ -126,7 +140,6 @@ class State:
                 news.d[i]['cases'] -= news.d[i]['deaths']
 
         return news
-            
 
 
 def goal_test(s):
@@ -154,6 +167,7 @@ class Operator:
     def apply(self, s):
         return self.state_transf(s)
 
+
 actions = ['Research', 'Drugs', 'Education']
 combinations = [('Research', 8), ('nothing', 9)]
 for a in ['Drugs', 'Education']:
@@ -161,12 +175,12 @@ for a in ['Drugs', 'Education']:
         combinations.append((a, i))
 
 OPERATORS = [Operator('Invest in ' + a + ' for ' + REGIONS[loc] + ' ($' + str(action_costs[a]) + ')',
-                      lambda s,a1=a,l1=loc: s.can_move(a1, l1),
+                      lambda s, a1=a, l1=loc: s.can_move(a1, l1),
                       # The default value construct is needed
                       # here to capture the values of p&q separately
                       # in each iteration of the list comp. iteration.
-                      lambda s,a1=a,l1=loc: s.move(a1, l1) )
-             for (a,loc) in combinations]
+                      lambda s, a1=a, l1=loc: s.move(a1, l1))
+             for (a, loc) in combinations]
 
 # <GOAL_TEST> (optional)
 GOAL_TEST = lambda s: goal_test(s)
